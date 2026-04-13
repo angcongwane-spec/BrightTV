dc7f1170fb0fe01e19c87b25f8e69aedfa6ab9e1
Andile Ngcongwane <angcongwane@gmail.com>
2026-04-13 16:43:31 +0200

    Add SQL queries for user profiles and viewerships
    Added SQL queries to view user profiles and viewerships, including a full outer join.

--- ViewershipAnalytics.sql
+++ ViewershipAnalytics.sql
@@ -1,11 +1,14 @@
+--Viewing all the columns of the UserProfile table
 select *
 from `brighttv`.`tv`.`user_profiles`
 limit 10;
 
+--Viewing all the columns of the Viewerships table
 select*
 from `brighttv`.`tv`.`viewerships`
 limit 10;
 
+--Joining only the important columns from both tables using a full outer join
 select a.UserID, b.UserID, Gender, Race, Age, Province, Channel, RecordDate
 from `brighttv`.`tv`.`user_profiles` as a
 full outer join `brighttv`.`tv`.`viewerships` as b
