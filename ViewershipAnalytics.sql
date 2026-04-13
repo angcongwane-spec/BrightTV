351389c714c6d0b756e10ea13223e4d41f0de791
Andile Ngcongwane <angcongwane@gmail.com>
2026-04-13 22:54:43 +0200

    Add NULL replacement queries for user_profiles and viewerships
    Added queries to replace NULL values in user_profiles and viewerships tables.

--- ViewershipAnalytics.sql
+++ ViewershipAnalytics.sql
@@ -22,6 +22,20 @@ from brighttv.tv.user_profiles;
 select count (*)
 from brighttv.tv.viewerships;
 
+--Checking for and replacing NULLS in the table user_profile
+select ifnull (Gender, 'no_gender_selected') as Gender,
+ifnull (Race, 'no_race_selected') as Race,
+ifnull (Age, 'no_age_selected') as Age,
+ifnull (Province, 'no_province_selected') as Province
+from brighttv.tv.user_profiles;
+
+--Checking for and replacing NULLS in the table viewerships
+select ifnull (Channel, 'no_channel_selected') as Channel,
+ifnull (RecordDate, 'no_record_date_selected') as RecordDate,
+ifnull (Duration, 'no_duration_selected') as Duration
+from brighttv.tv.viewerships;
+
+
 --Checking the uniques Provinces
 select DISTINCT Province
 from `brighttv`.`tv`.`user_profiles`;
